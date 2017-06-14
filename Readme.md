20fd8b6cf6c3f11a0e837a37ae4f2b8e74a1919f
spoclab <spoclab@users.noreply.github.com>
2017-06-14 12:51:23 +0200

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,24 @@
+SPOClab-Syringe Pump Software for DropletKitchen Pumps
+author: Christian Arrer; christianarrer@gmail.com
+
+1.Introduction
+This is a software to operate the DropletKitchen Syringe Pumps (https://github.com/DropletKitchen/pumpsn17). 
+The software consists of two parts: The part runs on the Teensy Board (2.0/3.0) and the second part runs on Windows. 
+The Teensy Board software is in “teensy/product762/product762.ino” as an Arduino file. 
+The PC software is a Visual Studio Community 2017 Project and can be found inside “pc/prodcut762/product762.sln”.
+Install the latest Arduino (https://www.arduino.cc/en/Main/OldSoftwareReleases) and Teensyduino 
+(https://www.pjrc.com/teensy/td_download.html) on Windows.
+Install Visual Studio Commmunity 2017 
+(https://www.visualstudio.com/de/thank-you-downloading-visual-studio/?sku=Community&rel=15) and 
+make sure to include all Visual C# features.
+
+1.1	Compilation for Teensy Board
+Start Arduino, go to “tools->board” and choose “Teensy 2.0” or “Teensy 3.0”, depending on which board you got. 
+Then go to “sketch->upload” or press “ctrl-U” or press the “arrow button” to upload the code on your USB connected Teensy Board.
+
+1.2	Compile Windows Software
+Open up the Visual Studio Community 2017 C# IDE, then open the file “pc/prodcut762/product762.sln”. 
+In the “Standard”/”Default” Menustrip 
+Switch to compile for “Release”, this will strip the debug overhead from the executable. 
+Then go to “build->rebuild solution”.  Now you should have a “porduct762.exe” in “pc\product762\bin\Release” folder. 
+You can launch it on any PC with .NET Framework 4.6 installed.
---
+++ product762.ino
@@ -0,0 +1,78 @@
+#define RECV_OK 42
+#define STATE_MSG_LEN 32 // 32 bytes (four times double float, each has 8 bytes) + 1 (direction flags byte) = 33
+#define STRANGE_FACTOR // = 1000000 / 16 = 62500.00; 1000000 is the conversion seconds to µs; 1/16 is the strange factor
+union unionStateFreq
+{
+  double doubleValues[4];
+  byte byteValues[32];
+} stateFreqsRead;
+double stateFreqs[4];
+int stateFreqsCurByte = 0;
+byte stateDirectionFlags = 0; // all rotate right
+double lastTimes[4];
+int dirPins[4] = {0, 2, 4, 6};
+int stepPins[4] = {1, 3, 5, 7};
+byte logMasks[4] = {8, 4, 2, 1}; // start with DEC 8 = BIN 0000 1000, DEC 4 = 0000 0100, DEC 2 = 0000 0010, DEC 1 = 0000 0001;
+
+void setup() {
+  Serial.begin(9600);
+  lastTimes[0] = lastTimes[1] = lastTimes[2] = lastTimes[3] = micros(); // init the last time var
+  stateFreqs[0] = stateFreqs[1] = stateFreqs[2] = stateFreqs[3] = 0;
+  stateFreqsRead.doubleValues[0] = stateFreqsRead.doubleValues[1] = stateFreqsRead.doubleValues[2] = stateFreqsRead.doubleValues[3] = 0;
+  for(unsigned int i = 0; i < 4; i++)
+  {
+    pinMode(dirPins[i], OUTPUT);
+    pinMode(stepPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    if(stateFreqsCurByte < STATE_MSG_LEN) // bytes 0 - 31 (32) are the 4 floating point numbers
+    {      
+      stateFreqsRead.byteValues[stateFreqsCurByte] = Serial.read();
+      stateFreqsCurByte++;
+    } 
+    else if(stateFreqsCurByte == STATE_MSG_LEN) // the 32rd (33rd) byte is  the direction flags
+    {
+      stateDirectionFlags = Serial.read();
+      Serial.write(RECV_OK); // write recv ok
+      updateDirAndFreq(); // update frequencies and directions  
+      stateFreqsCurByte = 0;
+    }
+  }
+  for(int i = 0; i < 4; i++)
+  {
+    if(micros() < lastTimes[i]) // micros will restart ~ every 71 minutes, if we yust stepped in this gap, simply restart
+    {
+       lastTimes[i] = micros();
+    }
+    else
+    {
+      if(((micros() - lastTimes[i]) * stateFreqs[i]) > 62500.0)
+      {        
+        lastTimes[i] = micros();
+        digitalWrite(stepPins[i], 1); //step-pin on for 1 µs
+        delayMicroseconds(1); //step-pin on for 1 µs
+        digitalWrite(stepPins[i], 0); //step-pin on for 1 µs
+      }
+    }
+  }
+}
+
+void updateDirAndFreq()
+{
+  for(unsigned int i = 0; i < 4; i++)
+  {
+    stateFreqs[i] = stateFreqsRead.doubleValues[i]; // update frequencies
+    if((stateDirectionFlags & logMasks[i]) == logMasks[i]) // bit wise & the directions flags bit set is clockwise (left rotation)
+    {
+      digitalWriteFast(dirPins[i], 1); // rotate clockwise (left)
+    }
+    else
+    {
+      digitalWriteFast(dirPins[i], 0); // rotate counter clockwise (right)
+    }
+  }  
+}
+
